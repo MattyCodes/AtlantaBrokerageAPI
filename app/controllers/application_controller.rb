ff0a8ac1f541341a61906d7ddb864a1defa54f14
MattyCodes <matt.nick.fagioli@gmail.com>
2020-01-26 13:03:28 -0500

    Some style updates and comments and cleanup.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,28 @@
 class ApplicationController < ActionController::API
   before_action :validate_request_host!
 
+  # This whitelist determines which controllers a domain is authorized to 
+  # interact with. For example the domain "free-atlanta-move.herokuapp.com" is
+  # only authorized to interact with the `FreeAtlantaMoveController`.
   CONTROLLER_DOMAIN_WHITELIST = {
     'free_atlanta_move' => 'free-atlanta-move.herokuapp.com'
   }
 
   def validate_request_host!
-    invalid_request! unless requesting_domain.present? && CONTROLLER_DOMAIN_WHITELIST[controller_name].include?(requesting_domain)
+    invalid_request! unless the_request_is_from_an_authorized_domain?
+  end
+
+  def the_request_is_from_an_authorized_domain?
+    requesting_domain.present? && 
+      CONTROLLER_DOMAIN_WHITELIST[controller_name].include?(requesting_domain)
   end
 
   def invalid_request!
-    render json: { success: false, body: 'This request is invalid, please ensure that it is being sent from the appropriate domain.' } and return
+    render json: { 
+      success: false,
+      status: 402,
+      body: 'Unauthorized domain.' 
+    }
   end
 
   def controller_name
--- spec/controllers/v1/free_atlanta_move_controller_spec.rb
+++ spec/controllers/v1/free_atlanta_move_controller_spec.rb
@@ -3,17 +3,26 @@ require 'rails_helper'
 RSpec.describe V1::FreeAtlantaMoveController, type: :controller do
   TEST_CONTROLLER_DOMAIN_WHITELIST = { 'free_atlanta_move' => 'example.com' }
 
+  before :each do
+    stub_const(
+      'ApplicationController::CONTROLLER_DOMAIN_WHITELIST',
+      TEST_CONTROLLER_DOMAIN_WHITELIST
+    )
+  end
+
   it 'Responds with a success JSON when the request host is valid.' do
-    allow_any_instance_of(ActionController::TestRequest).to receive(:referer).and_return('https://example.com')
-    stub_const('ApplicationController::CONTROLLER_DOMAIN_WHITELIST', TEST_CONTROLLER_DOMAIN_WHITELIST)
+    allow_any_instance_of(ActionController::TestRequest)
+      .to receive(:referer).and_return('https://example.com')
+
     post :home_form_submission, params: { email: 'testing@example.com' }
     expect(response).to be_successful
     expect(JSON.parse(response.body)['success']).to eq true
   end
 
   it 'Responds with a success JSON when the request host is valid.' do
-    allow_any_instance_of(ActionController::TestRequest).to receive(:referer).and_return('invalid.host')
-    stub_const('ApplicationController::CONTROLLER_DOMAIN_WHITELIST', TEST_CONTROLLER_DOMAIN_WHITELIST)
+    allow_any_instance_of(ActionController::TestRequest)
+      .to receive(:referer).and_return('invalid.host')
+
     post :home_form_submission
     expect(response).to be_successful
     expect(JSON.parse(response.body)['success']).to eq false
